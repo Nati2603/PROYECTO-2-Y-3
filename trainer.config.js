470110fe1865394b08afd006bce31a211882ca76
Nati2603 <nati.m.26366@gmail.com>
2026-04-03 21:46:50 -0600

    Add pokemón Mew

---
+++ trainer.config.js
@@ -0,0 +1,17 @@
+const TRAINER = {
+  name: "Natalia",
+  hometown: "Heredia",
+  catchphrase: "¡Vamos a ganar!",
+
+  favoritePokemon: "mew",
+  nickname: "Mewito",
+  battleCry: "Mew Mew!",
+
+  definitiveMoveName: "Psychic Storm",
+  definitiveMoveFlavor: "Una explosión mental imposible de esquivar.",
+
+  winMessage: "¡Mew! Sabía que podíamos lograrlo.",
+  loseMessage: "La próxima vez será mewjor."
+};
+
+export default TRAINER;
